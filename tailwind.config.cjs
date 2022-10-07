6cde5ce352d0982998ebc48702728f6b099269d6
TheBarbellCoder <avinash.ravishankar@thebarbellcoder.com>
2022-10-07 20:51:22 +0200

    created landing page

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,13 @@
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+	    colors: {
+	        'gray-light' : '#edefee',
+	        'orange' : '#d08856',
+	        'red' : '#aa210f',
+	        'gray-dark' : '#41403c',
+	        },
+		extend: {}
 	},
 	plugins: [],
 }
