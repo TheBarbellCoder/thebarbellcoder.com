2eaf67dec159d83e2322cec9435644b9751fe613
TheBarbellCoder <avinash.ravishankar@thebarbellcoder.com>
2022-10-08 17:32:03 +0200

    cleaned branch

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,7 @@
 import { defineConfig } from 'astro/config';
-
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
-  site: 'https://thebarbellcoder.com'
+  integrations: [tailwind()]
 });
-
