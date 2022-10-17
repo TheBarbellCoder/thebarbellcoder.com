8e79d0da450c1ea6d4e72f101fbd4b12624063d8
Avinash Chikkadlur Ravi Shankar <20731240+TheBarbellCoder@users.noreply.github.com>
2022-10-17 19:19:37 +0200

    3 setup and style markdown for blogs (#4)
    * cleaned branch
    
    * styled markdown files for blogs
    
    * added kicker and image banner
    
    Co-authored-by: TheBarbellCoder <avinash.ravishankar@thebarbellcoder.com>

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,16 @@
 import { defineConfig } from 'astro/config';
-
 import tailwind from "@astrojs/tailwind";
+
+import image from "@astrojs/image";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
-  site: 'https://thebarbellcoder.com'
+  integrations: [tailwind(), image({serviceEntryPoint: '@astrojs/image/sharp'})],
+  vite: {ssr: {external: ["svgo"]}},
+  markdown: {
+    shikiConfig: {
+      theme: 'css-variables',
+      wrap: false
+    }
+  }
 });
-
