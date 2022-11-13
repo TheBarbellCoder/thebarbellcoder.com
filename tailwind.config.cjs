408f14ce75f0285010db97a5a8e3eafb6aaff9a5
Avinash Chikkadlur Ravi Shankar <20731240+TheBarbellCoder@users.noreply.github.com>
2022-11-13 08:14:32 +0100

    added unittest blog and image files (#10)
    Co-authored-by: TheBarbellCoder <avinash@thebarbellcoder.com>

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,93 +2,92 @@
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-	    colors: {
-	        'gray': {
-	            'light': '#edefee',
-	            'dark': '#41403c'
-	        },
-	        'orange': '#d08856',
-	        'red': '#aa210f',
+	  colors: {
+	    'gray': {
+	      'light': '#edefee',
+	      'dark': '#41403c'
 	    },
-	    fontFamily: {
-	        'sans': ['Lato'],
-	        'serif': ['"Libre Baskerville"'],
-	        'mono': ['"IBM Plex Mono"']
-	    },
+	    'orange': '#d08856',
+	    'red': '#aa210f',
+	  },
+	  fontFamily: {
+	    'sans': ['Lato'],
+	    'serif': ['"Libre Baskerville"'],
+	    'mono': ['"IBM Plex Mono"']
+	  },
 		extend: {
-		    typography: theme => ({
-		        DEFAULT: {
-		            css: {
+		  typography: theme => ({
+		    DEFAULT: {
+		      css: {
 
-                        '--tw-prose-headings': theme('colors.gray.dark'),
-                        '--astro-code-color-text': theme('colors.gray.dark'),
-                        '--astro-code-color-background': '#edefee80',
-                        '--astro-code-token-comment': '#568c3e',
-                        '--astro-code-token-keyword': '#3e568c',
-                        '--astro-code-token-constant': theme('colors.gray.dark'),
-                        '--astro-code-token-string-expression': '#bd5533',
-                        '--astro-code-token-punctuation': theme('colors.gray.dark'),
-                        '--astro-code-token-function': theme('colors.orange'),
+            '--tw-prose-headings': theme('colors.gray.dark'),
+            '--tw-prose-td-borders': theme('colors.gray.light'),
+            '--astro-code-color-text': theme('colors.gray.dark'),
+            '--astro-code-color-background': '#edefee80',
+            '--astro-code-token-comment': '#568c3e',
+            '--astro-code-token-keyword': '#3e568c',
+            '--astro-code-token-constant': theme('colors.gray.dark'),
+            '--astro-code-token-string-expression': '#bd5533',
+            '--astro-code-token-punctuation': theme('colors.gray.dark'),
+            '--astro-code-token-function': theme('colors.orange'),
 
-                        h1: {
-                            fontSize: '3rem',
-                            lineHeight: '1',
-                            fontFamily: theme('fontFamily.sans')
-                        },
-                        h2: {
-                            fontSize: '2.25rem',
-                            lineHeight: '2.5rem',
-                            fontFamily: theme('fontFamily.sans')   
-                        },
-                        h3: {
-                            fontSize: '1.5rem',
-                            lineHeight: '2rem',
-                            fontFamily: theme('fontFamily.sans')
-                        },
-                        h4: {
-                            fontSize: '1.25rem',
-                            lineHeight: '1.75rem',
-                            fontFamily: theme('fontFamily.sans')
-                        },
-                        'ol, ul, p': {
-                            fontSize: '1.125rem',
-                            lineheight: '1.75rem',
-                            fontFamily: theme('fontFamily.serif')
-                        },
-                        'li::marker': {
-                            color: theme('colors.gray.dark')
-                        },
-                        a: {
-                            textDecorationLine: 'underline',
-                            textUnderlineOffset: '4px',
-                            textDecorationThickness: 'auto',
-                            '&:hover': {
-                                textDecorationColor: theme('colors.orange'),
-                                textUnderlineOffset: '8px',
-                                textDecorationThickness: '2px'
-                            }
-                        },
-                        pre: {
-                            fontWeight: '500'
-                        },
-                        code: {
-                            backgroundColor: theme('colors.gray.light'),
-                            color: theme('colors.gray.dark'),
-                            fontFamily: theme('fontFamily.mono'),
-                            fontWeight: '500'
-                        },
-                        'code::before': {
-                            content: '""',
-                            paddingLeft: '0.25rem'
-                        },
-                        'code::after': {
-                            content: '""',
-                            paddingRight: '0.25rem'
-                        }
-                    }
-                }    
-            })
-        },
+            h1: {
+              fontSize: '3rem',
+              lineHeight: '1',
+              fontFamily: theme('fontFamily.sans')
+            },
+            h2: {
+              fontSize: '2.25rem',
+              lineHeight: '2.5rem',
+              fontFamily: theme('fontFamily.sans')   
+            },
+            h3: {
+              fontSize: '1.5rem',
+              lineHeight: '2rem',
+              fontFamily: theme('fontFamily.sans')
+            },
+            h4: {
+              fontSize: '1.25rem',
+              lineHeight: '1.75rem',
+              fontFamily: theme('fontFamily.sans')
+            },
+            'ol, ul, p': {
+              fontSize: '1.25rem',
+              lineheight: '1.75rem',
+              fontFamily: theme('fontFamily.serif')
+            },
+            'li::marker': {
+              color: theme('colors.gray.dark')
+            },
+            a: {
+              textDecorationLine: 'underline',
+              textUnderlineOffset: '4px',
+              textDecorationThickness: 'auto',
+              '&:hover': {
+                textUnderlineOffset: '8px',
+                textDecorationThickness: '2px'
+              }
+            },
+            pre: {
+              fontWeight: '500'
+            },
+            code: {
+              // backgroundColor: theme('colors.gray.light'),
+              color: theme('colors.gray.dark'),
+              fontFamily: theme('fontFamily.mono'),
+              fontWeight: '500',
+              fontSize: 'inherit'
+            },
+            'code::before': {
+              content: 'none'
+            },
+            'code::after': {
+              content: 'none',
+            }
+          }
+        }    
+      })
     },
+  },
 	plugins: [require('@tailwindcss/typography'),],
 }
